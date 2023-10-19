920dc0070ad7d329f6cc8ec6e4f74506b608f67e
Murat Can ASLAN <muratcan.aslan@foreks.com>
2023-10-20 01:27:41 +0300

    Add icons.

---
+++ movie_papcorns_ios/movie_papcorns_ios/Managers/ImageManager.swift
@@ -0,0 +1,27 @@
+//
+//  ImageManager.swift
+//  movie_papcorns_ios
+//
+//  Created by Murat Can ASLAN on 20.10.2023.
+//
+
+import UIKit
+
+enum I {
+    
+    ///Tab icons
+    static let iconDiscoverySelected = UIImage(named: "iconDiscoverySelected")
+    static let iconDiscoveryUnselected = UIImage(named: "iconDiscoveryUnselected")
+    
+    static let iconFavoriteSelected = UIImage(named: "iconFavoriteSelected")
+    static let iconFavoriteUnselected = UIImage(named: "iconFavoriteUnselected")
+    
+    //General icons
+    static let iconBack = UIImage(named: "iconBack")
+ 
+    static let iconLike = UIImage(named: "iconLike")
+    static let iconDislike = UIImage(named: "iconDislike")
+
+    static let iconFavorite = UIImage(named: "iconFavorite")
+    static let iconUnfavorite = UIImage(named: "iconUnfavorite")
+}
