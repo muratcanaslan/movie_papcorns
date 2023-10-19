7ef6b8a2b73756f6326170519ee2f83476d653dc
Murat Can ASLAN <muratcan.aslan@foreks.com>
2023-10-20 01:14:26 +0300

    Edit folder structures.

---
+++ movie_papcorns_ios/movie_papcorns_ios/APP/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  movie_papcorns_ios
+//
+//  Created by Murat Can ASLAN on 20.10.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
